7527e78308bd94f287873684093fca1aecfa470c
Matthew-Mokhles <Matthewmokhles@gmail.com>
2024-12-29 02:18:08 +0200

    login redesign

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import '../services/auth_service.dart';
 import 'home_screen.dart';
@@ -54,24 +56,47 @@ class _SignInScreenState extends State<SignInScreen> {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: SafeArea(
-        child: Center(
+      body: Stack(children: [
+        // Background Image
+        Positioned.fill(
+          child: Image.asset(
+            'assets/images/Untitled-1.png', // Replace with the path to your image
+            fit: BoxFit.cover, // To make the image cover the whole screen
+          ),
+        ),
+
+        // Backdrop Filter for blur effect
+        Positioned.fill(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+                sigmaX: 2.0, sigmaY: 2.0), // Adjust the blur strength here
+            // child: Container(
+            //   color: const Color.fromARGB(255, 132, 255, 163).withOpacity(0.4), // Optional: adds a slight dark overlay
+            // ),
+          ),
+        ),
+        // Content on top of the blurred background
+        Center(
           child: SingleChildScrollView(
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0),
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
               child: Container(
-                width: screenWidth,
-                height: screenHeight,
+                width: screenWidth * 0.8,
+                height: screenHeight * 0.6,
                 padding: EdgeInsets.all(screenWidth * 0.05),
                 decoration: BoxDecoration(
-                  color: Colors.green.shade400,
-                  borderRadius: BorderRadius.circular(15.0),
+                  gradient: LinearGradient(
+                    colors: [Colors.green.shade400, Colors.green.shade700],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(30.0),
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
+                      color: Colors.black.withOpacity(0.3),
                       blurRadius: 10,
                       spreadRadius: 2,
-                      offset: const Offset(0, 5),
+                      offset: Offset(0, 5),
                     ),
                   ],
                 ),
@@ -81,25 +106,24 @@ class _SignInScreenState extends State<SignInScreen> {
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       const Text(
-                        "Welcome!",
+                        "Welcome Back!",
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 40,
+                          fontSize: 36,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
                       const SizedBox(height: 20),
                       const Text(
-                        "LOGIN",
+                        "Login to Your Account",
                         style: TextStyle(
                           color: Colors.white,
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
-                      const SizedBox(height: 24),
-                      _buildTextFormField(
-                          "Email", _emailTextController, false),
+                      const SizedBox(height: 40),
+                      _buildTextFormField("Email", _emailTextController, false),
                       const SizedBox(height: 24),
                       _buildTextFormField(
                           "Password", _passwordTextController, true),
@@ -120,7 +144,7 @@ class _SignInScreenState extends State<SignInScreen> {
                           ),
                         ),
                       ),
-                      const SizedBox(height: 10),
+                      const SizedBox(height: 20),
                       ElevatedButton(
                         onPressed: _isLoading ? null : _signIn,
                         style: ElevatedButton.styleFrom(
@@ -129,20 +153,21 @@ class _SignInScreenState extends State<SignInScreen> {
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(30.0),
                           ),
-                          padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          padding: const EdgeInsets.symmetric(vertical: 14.0),
+                          elevation: 6,
                         ),
                         child: _isLoading
                             ? const CircularProgressIndicator(
-                          color: Colors.green,
-                        )
+                                color: Colors.green,
+                              )
                             : const Text("Login"),
                       ),
-                      const SizedBox(height: 15),
+                      const SizedBox(height: 25),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           const Text(
-                            "Don't have an account?",
+                            "Don't have an account? ",
                             style: TextStyle(color: Colors.white),
                           ),
                           GestureDetector(
@@ -154,10 +179,11 @@ class _SignInScreenState extends State<SignInScreen> {
                               );
                             },
                             child: const Text(
-                              " Sign Up",
+                              "Sign Up",
                               style: TextStyle(
                                 color: Colors.white,
                                 fontWeight: FontWeight.bold,
+                                decoration: TextDecoration.underline,
                               ),
                             ),
                           ),
@@ -170,34 +196,62 @@ class _SignInScreenState extends State<SignInScreen> {
             ),
           ),
         ),
-      ),
+      ]),
     );
   }
 
+  bool _isPasswordVisible =
+      false; // State variable to toggle password visibility
+
   Widget _buildTextFormField(
-      String labelText, TextEditingController controller, bool isPassword) {
+      String labelText, TextEditingController controller, bool obscureText) {
     return TextFormField(
       controller: controller,
-      obscureText: isPassword,
+      obscureText: obscureText && !_isPasswordVisible, // Toggle visibility
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return '$labelText cannot be empty';
+        }
+        return null;
+      },
+      style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
         labelText: labelText,
+        labelStyle: TextStyle(color: Colors.white.withOpacity(0.7)),
+        prefixIcon: Icon(
+          labelText == "Email" ? Icons.email : Icons.lock,
+          color: Colors.white.withOpacity(0.7),
+        ),
+        suffixIcon: labelText.contains("Password")
+            ? IconButton(
+                icon: Icon(
+                  _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  color: Colors.white.withOpacity(0.7),
+                ),
+                onPressed: () {
+                  // Toggle the password visibility
+                  setState(() {
+                    _isPasswordVisible = !_isPasswordVisible;
+                  });
+                },
+              )
+            : null, // Add suffix icon only for password fields
         filled: true,
-        fillColor: Colors.white,
+        fillColor: Colors.green.shade800,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(30.0),
           borderSide: BorderSide.none,
         ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(
+            color: Colors.white, // Border color when focused
+            width: 2.0,
+          ),
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(vertical: 18.0, horizontal: 20.0),
       ),
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return "$labelText cannot be empty";
-        }
-        if (labelText == "Email" &&
-            !RegExp(r'^[^@]+@[^@]+\.[^@]+').hasMatch(value)) {
-          return "Enter a valid email";
-        }
-        return null;
-      },
     );
   }
 }
--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'home_screen.dart';
 import 'login_screen.dart';
+import 'dart:ui'; // For ImageFilter.blur
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -15,7 +16,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final TextEditingController _passwordTextController = TextEditingController();
   final TextEditingController _emailTextController = TextEditingController();
   final TextEditingController _userNameTextController = TextEditingController();
-  final TextEditingController _rePasswordTextController = TextEditingController();
+  final TextEditingController _rePasswordTextController =
+      TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   bool isCitizen = false;
@@ -34,8 +36,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
       });
 
       try {
-        UserCredential userCredential = await FirebaseAuth.instance
-            .createUserWithEmailAndPassword(
+        UserCredential userCredential =
+            await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: _emailTextController.text.trim(),
           password: _passwordTextController.text.trim(),
         );
@@ -94,176 +96,244 @@ class _SignUpScreenState extends State<SignUpScreen> {
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0),
-            child: Container(
-              width: screenWidth,
-              height: screenHeight,
-              padding: EdgeInsets.all(screenWidth * 0.05),
-              decoration: BoxDecoration(
-                color: Colors.green.shade400,
-                borderRadius: BorderRadius.circular(15.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 10,
-                    spreadRadius: 2,
-                    offset: const Offset(0, 5),
+      body: Stack(
+        children: [
+          // Background Image
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/Untitled-1.png', // Replace with the path to your image
+              fit: BoxFit.cover, // To make the image cover the whole screen
+            ),
+          ),
+
+          // Backdrop Filter for blur effect
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(
+                  sigmaX: 2.0, sigmaY: 2.0), // Adjust the blur strength here
+              // child: Container(
+              //   color: const Color.fromARGB(255, 132, 255, 163).withOpacity(0.4), // Optional: adds a slight dark overlay
+              // ),
+            ),
+          ),
+          // Content on top of the blurred background
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: screenWidth * 0.05,
+                    vertical: screenHeight * 0.05),
+                child: Container(
+                  width: screenWidth * 1.0,
+                  height: screenHeight * 0.75,
+                  padding: EdgeInsets.all(screenWidth * 0.05),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Colors.green.shade400, Colors.green.shade700],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                    borderRadius: BorderRadius.circular(30.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.3),
+                        blurRadius: 15,
+                        spreadRadius: 2,
+                        offset: Offset(0, 10),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              child: Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Welcome!",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      "SIGN UP",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    _buildTextFormField("Full Name", _userNameTextController, false),
-                    const SizedBox(height: 15),
-                    _buildTextFormField("Email", _emailTextController, false),
-                    const SizedBox(height: 15),
-                    _buildTextFormField("Password", _passwordTextController, true),
-                    const SizedBox(height: 15),
-                    _buildTextFormField(
-                      "Re-Enter Password",
-                      _rePasswordTextController,
-                      true,
-                    ),
-                    const SizedBox(height: 20),
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text(
-                        "User Type",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                        ),
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Checkbox(
-                          value: isCitizen,
-                          onChanged: (value) {
-                            setState(() {
-                              isCitizen = value!;
-                              isCompany = false;
-                            });
-                          },
-                          activeColor: Colors.white,
-                          checkColor: Colors.green,
-                        ),
-                        const Text(
-                          "Citizen",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                          ),
-                        ),
-                        const SizedBox(width: 20),
-                        Checkbox(
-                          value: isCompany,
-                          onChanged: (value) {
-                            setState(() {
-                              isCompany = value!;
-                              isCitizen = false;
-                            });
-                          },
-                          activeColor: Colors.white,
-                          checkColor: Colors.green,
-                        ),
-                        const Text(
-                          "Company",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 18,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: _isLoading ? null : _signUp,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.green.shade400,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 12.0),
-                      ),
-                      child: _isLoading
-                          ? const CircularProgressIndicator(color: Colors.green)
-                          : const Text("Sign Up"),
-                    ),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         const Text(
-                          "Already have an account?",
-                          style: TextStyle(color: Colors.white),
+                          "Welcome!",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignInScreen()),
-                            );
-                          },
-                          child: const Text(
-                            " Login",
+                        const SizedBox(height: 20),
+                        const Text(
+                          "SIGN UP",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        _buildTextFormField(
+                            "Full Name", _userNameTextController, false),
+                        const SizedBox(height: 15),
+                        _buildTextFormField(
+                            "Email", _emailTextController, false),
+                        const SizedBox(height: 15),
+                        _buildTextFormField(
+                            "Password", _passwordTextController, true),
+                        const SizedBox(height: 15),
+                        _buildTextFormField(
+                          "Re-Enter Password",
+                          _rePasswordTextController,
+                          true,
+                        ),
+                        const SizedBox(height: 20),
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "User Type",
                             style: TextStyle(
                               color: Colors.white,
-                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
                             ),
                           ),
                         ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Checkbox(
+                              value: isCitizen,
+                              onChanged: (value) {
+                                setState(() {
+                                  isCitizen = value!;
+                                  isCompany = false;
+                                });
+                              },
+                              activeColor: Colors.white,
+                              checkColor: Colors.green,
+                            ),
+                            const Text(
+                              "Citizen",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                            const SizedBox(width: 20),
+                            Checkbox(
+                              value: isCompany,
+                              onChanged: (value) {
+                                setState(() {
+                                  isCompany = value!;
+                                  isCitizen = false;
+                                });
+                              },
+                              activeColor: Colors.white,
+                              checkColor: Colors.green,
+                            ),
+                            const Text(
+                              "Company",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: _isLoading ? null : _signUp,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.green.shade400,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 12.0),
+                          ),
+                          child: _isLoading
+                              ? const CircularProgressIndicator(
+                                  color: Colors.green)
+                              : const Text("Sign Up"),
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              "Already have an account?",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SignInScreen()),
+                                );
+                              },
+                              child: const Text(
+                                " Login",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ],
-                    )
-                  ],
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
+        ],
       ),
     );
   }
+
+  bool _isPasswordVisible =
+      false; // State variable to toggle password visibility
 
   Widget _buildTextFormField(
       String labelText, TextEditingController controller, bool isPassword) {
     return TextFormField(
       controller: controller,
-      obscureText: isPassword,
+      obscureText: isPassword && !_isPasswordVisible, // Toggle visibility
       decoration: InputDecoration(
         labelText: labelText,
+        labelStyle: TextStyle(
+          color: Colors.white
+              .withOpacity(0.7), // Set your desired label color here
+          fontSize: 16.0, // Optional: Adjust font size
+          fontWeight: FontWeight.w500, // Optional: Adjust font weight
+        ),
         filled: true,
-        fillColor: Colors.white,
+        fillColor: Colors.green.shade800,
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(30.0),
           borderSide: BorderSide.none,
         ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: const BorderSide(
+            color: Colors.white, // Border color when focused
+            width: 2.0,
+          ),
+        ),
+        suffixIcon: isPassword
+            ? IconButton(
+                icon: Icon(
+                  _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  color: Colors.white.withOpacity(0.7), // Icon color
+                ),
+                onPressed: () {
+                  // Toggle the password visibility
+                  setState(() {
+                    _isPasswordVisible = !_isPasswordVisible;
+                  });
+                },
+              )
+            : null, // Add suffix icon only for password fields
+      ),
+      style: const TextStyle(
+        color: Colors.white, // Text color inside the input
       ),
       validator: (value) {
         if (value == null || value.isEmpty) {
